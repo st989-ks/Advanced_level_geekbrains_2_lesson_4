08d0c8a3bb52d0aab2825efd32f969d140d510bf
Stanislav <st989@ya.ru>
2020-10-03 03:23:14 +0400

    чат

--- src/task_lesson_4/Main.java
+++ src/task_lesson_4/Main.java
@@ -1,15 +1,33 @@
 package task_lesson_4;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private int WIDTH = 600;
+    private int HEIGHT = 700;
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage chatPanel) throws Exception {
+
+        Parent root = FXMLLoader.load( getClass().getResource( "windows.fxml" ) );
+        chatPanel.setTitle( "Chat-Chat" );
+        chatPanel.setScene( new Scene( root, WIDTH, HEIGHT ) );
+//        chatPanel.setMinWidth(WIDTH);
+//        chatPanel.setMinHeight(HEIGHT);
+        chatPanel.setResizable(false);
+        chatPanel.show();
+
+//        chatPanel.minHeightProperty().set( HEIGHT );
+//        chatPanel.minWidthProperty().set( WIDTH );
+
 
     }
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args)  {
+        launch(args);
     }
 }
